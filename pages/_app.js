592d985afb73e24e76fa555d7cf48a445ce23cdf
Gonzalo Vazquez <gonzalovazquez010@gmail.com>
2020-12-03 12:41:56 -0500

    Added styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,12 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable class-methods-use-this */
 /* istanbul ignore file */
-import React from 'react'
-import App from 'next/app'
-import Head from 'next/head'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
+import React from "react";
+import App from "next/app";
+import Head from "next/head";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import "../styles/styles.css";
 
 /**
  * Define theme for starter-kit
@@ -14,45 +15,63 @@ import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles'
  */
 const theme = createMuiTheme({
   palette: {
-    type: 'light',
-  },
-})
+    type: "light"
+  }
+});
 
 export default class NextAppTemplate extends App {
   static async getInitialProps({ Component, ctx }) {
-    let pageProps = {}
+    let pageProps = {};
 
     if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx)
+      pageProps = await Component.getInitialProps(ctx);
     }
 
-    return { pageProps }
+    return { pageProps };
   }
 
   renderHead() {
     return (
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png" />
-        <link rel="mask-icon" href="/static/safari-pinned-tab.svg" color="#000000" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/favicon-16x16.png"
+        />
+        <link
+          rel="mask-icon"
+          href="/static/safari-pinned-tab.svg"
+          color="#000000"
+        />
         <meta name="msapplication-TileColor" content="#000000" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
         />
       </Head>
-    )
+    );
   }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
     return (
       <MuiThemeProvider theme={theme}>
         {this.renderHead()}
         <CssBaseline />
         <Component {...pageProps} />
       </MuiThemeProvider>
-    )
+    );
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import PropTypes from 'prop-types'
-import Head from 'next/head'
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
 /** Material UI Components */
 import {
   Grid,
@@ -14,44 +14,44 @@ import {
   Card,
   CardContent,
   Box
-} from '@material-ui/core';
+} from "@material-ui/core";
 
 /** Custom Components */
-import todoAPI from '../api/todo'
-import TodoComponent from '../components/Todo/Todo'
-import veganIpsumGenerator from '../utils/veganipsumGenerator'
+import todoAPI from "../api/todo";
+import TodoComponent from "../components/Todo/Todo";
+import veganIpsumGenerator from "../utils/veganipsumGenerator";
 
 /** Mock */
-import fruits from '../mocks/fruits'
+import fruits from "../mocks/fruits";
 
 type Todos = {
-  userId: number
-  id: number
-  title: string
-  completed: boolean
-}
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
 
 /**
  * Main Index Page to render on homepage
  */
 function IndexPage() {
-  const [size, setSize] = React.useState('long')
-  const [repeat, setRepeat] = React.useState(5)
-  const [text, setText] = React.useState({ __html: ' ' })
+  const [size, setSize] = React.useState("long");
+  const [repeat, setRepeat] = React.useState(5);
+  const [text, setText] = React.useState({ __html: " " });
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSize((event.target as HTMLInputElement).value);
-  }
+  };
 
   const handleRepeat = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setRepeat(event.target.value)
-  }
+    setRepeat(event.target.value);
+  };
 
   const generateText = () => {
-    setText( { __html: ' ' } )
-    const generatedOutput = veganIpsumGenerator(size, repeat)
-    setText(generatedOutput)
-  }
+    setText({ __html: " " });
+    const generatedOutput = veganIpsumGenerator(size, repeat);
+    setText(generatedOutput);
+  };
 
   return (
     <React.Fragment>
@@ -60,48 +60,86 @@ function IndexPage() {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div style={{ width: '100%' }}>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
+      <div style={{ width: "100%" }}>
+        <Box display="flex" justifyContent="center" m={1} p={1}>
           <Box p={1} bgcolor="grey.300">
             <Typography variant="h1" component="h2" gutterBottom>
               Veganipsum
             </Typography>
           </Box>
         </Box>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
+        <Box display="flex" justifyContent="center" m={1} p={1}>
           <Box p={1} bgcolor="grey.300">
-          <Typography variant="body1" gutterBottom>
-            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos blanditiis tenetur
-            unde suscipit, quam beatae rerum inventore consectetur, neque doloribus, cupiditate numquam
-            dignissimos laborum fugiat deleniti? Eum quasi quidem quibusdam.
-          </Typography>
+            <Typography variant="h3" component="h3" gutterBottom>
+              Tired of how boring Lorem Ipsum got?
+            </Typography>
+            <Typography variant="body1" gutterBottom>
+              How about using auto-generated text that will actually make people
+              love your project even more?
+              <Typography variant="caption" display="block">
+                Pretty sweet, right?
+              </Typography>
+            </Typography>
+          </Box>
+          <Box p={1} bgcolor="grey.300"></Box>
+        </Box>
+        <Box
+          display="flex"
+          justifyContent="center"
+          m={1}
+          p={1}
+          bgcolor="background.paper"
+        >
+          <Box p={1} bgcolor="grey.300">
+            <Input
+              type="number"
+              defaultValue="5"
+              value={repeat}
+              onChange={handleRepeat}
+            />
           </Box>
         </Box>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
+        <Box display="flex" justifyContent="center" m={1} p={1}>
           <Box p={1} bgcolor="grey.300">
-            <Input type="number" defaultValue="5" value={repeat} onChange={handleRepeat}/>
-          </Box>
-        </Box>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
-          <Box p={1} bgcolor="grey.300">
-            <FormControl component="fieldset">
-              <RadioGroup aria-label="size" name="size" value={size} onChange={handleChange}>
-                <FormControlLabel value="Large" control={<Radio />} label="Large" />
-                <FormControlLabel value="Medium" control={<Radio />} label="Medium" />
-                <FormControlLabel value="Small" control={<Radio />} label="Small" />
+            <FormControl component="fieldset" id="size-selection">
+              <RadioGroup
+                aria-label="size"
+                name="size"
+                value={size}
+                onChange={handleChange}
+              >
+                <FormControlLabel
+                  value="Large"
+                  control={<Radio />}
+                  label="Large"
+                />
+                <FormControlLabel
+                  value="Medium"
+                  control={<Radio />}
+                  label="Medium"
+                />
+                <FormControlLabel
+                  value="Small"
+                  control={<Radio />}
+                  label="Small"
+                />
               </RadioGroup>
             </FormControl>
           </Box>
         </Box>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
-          <Box p={1} bgcolor="grey.300">
-            <Button variant="contained" color="primary" onClick={() => generateText() }>
+        <Box display="flex" justifyContent="center" m={1} p={1}>
+          <Box p={1}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => generateText()}
+            >
               Generate
             </Button>
           </Box>
         </Box>
-        <Box display="flex" justifyContent="center" m={1} p={1} bgcolor="background.paper">
-          <Box p={1} bgcolor="grey.300">
+        <Box display="flex" justifyContent="center" m={1} p={1}>
+          <Box p={1}>
             <Card>
               <CardContent>
                 <div dangerouslySetInnerHTML={text} />
@@ -111,7 +149,7 @@ function IndexPage() {
         </Box>
       </div>
     </React.Fragment>
-  )
+  );
 }
 
 /**
@@ -119,7 +157,7 @@ function IndexPage() {
  * @property {function} todoAPI - API to retrieve Todos.
  */
 IndexPage.propTypes = {
-  todoAPI: PropTypes.func,
-}
+  todoAPI: PropTypes.func
+};
 
-export default IndexPage
+export default IndexPage;
